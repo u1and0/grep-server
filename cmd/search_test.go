f2025d99b778763c15c7d500eba51c489088c639
u1and0 <e01.ando60@gmail.com>
2020-06-07 21:41:28 +0900

    [add] Success Test & Timeout Test @search_test.go

--- cmd/search_test.go
+++ cmd/search_test.go
@@ -8,39 +8,57 @@ import (
 
 func TestSearch_Grep(t *testing.T) {
 	var err error
-	// No keyrord
+	// No keyrord Error
 	s := Search{}
 	expectedErr := errors.New("検索キーワードがありません")
 	if _, err = s.Grep(); err == nil {
 		t.Errorf("This test must fail.\ngot: %v want: %v", err, expectedErr)
 	}
-	// No path
+	// No path Error
 	s = Search{Keyword: "hoge"}
 	expectedErr = errors.New("ディレクトリパスがありません")
 	if _, err = s.Grep(); err == nil {
 		t.Errorf("This test must fail.\ngot: %v want: %v", err, expectedErr)
 	}
-	// Not exist path
+	// Not exist path Error
 	s = Search{Keyword: "hoge", Path: "fuga", CmdPath: "foo"}
 	expectedErr = errors.New("ディレクトリパス " + s.CmdPath + " がありません")
 	if _, err = s.Grep(); err == nil {
 		t.Errorf("This test must fail.\ngot: %v want: %v", err, expectedErr)
 	}
-	// Timeout
+	// Timeout Error
 	s = Search{
-		Keyword:    "hoge",
-		Path:       "/var/log",
-		AndOr:      "and",
-		Depth:      "5",
-		Encoding:   "UTF-8",
-		CmdKeyword: "hoge",
-		CmdPath:    "foo",
-		Exe:        "/usr/bin/rga",
-		Timeout:    1 * time.Millisecond,
+		Keyword:  "bash",
+		Path:     "../test",
+		AndOr:    "and",
+		Depth:    "1",
+		Encoding: "UTF-8",
+		CmdPath:  "../test",
+		Exe:      "/usr/bin/rga",
+		Timeout:  1 * time.Nanosecond,
 	}
 	expectedErr = errors.New("タイムアウトしました。検索条件を変えてください。")
 	if _, err = s.Grep(); err == nil {
 		t.Errorf("This test must fail.\ngot: %v want: %v", err, expectedErr)
+	}
+	// Success
+	s = Search{
+		Keyword:  "bash",
+		Path:     "../test",
+		AndOr:    "and",
+		Depth:    "1",
+		Encoding: "UTF-8",
+		CmdPath:  "../test",
+		Exe:      "/usr/bin/rga",
+		Timeout:  10 * time.Second,
+	}
+	expected := 227 + // Matched lines
+		8 + // Stats lines
+		2 + // Filenames
+		2 // CRLF
+	actual, _ := s.Grep()
+	if expected != len(actual) {
+		t.Fatalf("got: %v want: %v", len(actual), expected)
 	}
 }
 
